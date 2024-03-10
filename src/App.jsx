0b138cd09d59da1d48584cab2f8e1c177d41763b
Nitin Sharma <85020631+nitin-sharma-7991@users.noreply.github.com>
2024-03-10 18:06:44 +0530

    todo app complete
    create update and deletion from localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { TodoProvider } from './Contexts'
+import TodoItem from './Components/TodoItem'
+import TodoForm from './Components/TodoForm'
 
 function App() {
   const [todos, setTodos] = useState([])
 
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id : Date.now(), ...todo}, ...prev ])
+  }
+
+  const updateTodo = (id,todo) => {
+    console.log('upodate')
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id ))
+  }
+  
+  const toogleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo  === id ? {...prevTodo, completed: !prevTodo.completed } : prevTodo))
+  }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if(todos && todos.length > 0 ) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos",JSON.stringify(todos))
+  }, [])
+
+ 
   return (
     <TodoProvider value={{todos ,addTodo, updateTodo, deleteTodo, toogleComplete}}>
     <div className="bg-[#172842] min-h-screen py-8">
@@ -12,9 +43,16 @@ function App() {
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm />
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map( (todo) => (
+                          <div key={todo.id}
+                          className='w-full'>
+                            <TodoItem todo={todo}/>
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>    
